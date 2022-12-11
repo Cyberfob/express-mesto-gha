f09fbbc0c374ef669e3b565d0fc297023b1a0d93
Cyberfob <apetruhin@global-rc.ru>
2022-12-12 02:18:59 +0300

    bugfix

--- validators/validator.js
+++ validators/validator.js
@@ -28,13 +28,13 @@ const celebrateUserMeAvatar = celebrate({
   }),
 });
 const celebrateCards = celebrate({
-  [Segments.BODY]: Joi.object().keys({
+  body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(linkAvatar),
+    link: Joi.string().required().pattern(regEx),
   }),
 });
 const celebrateCardsParam = celebrate({
-  [Segments.PARAMS]: Joi.object({
+  params: Joi.object({
     cardId: Joi.string().hex().length(24),
   }).required(),
 });
